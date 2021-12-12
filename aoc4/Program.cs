f4e0a8370faa3ff85b240723850fcd408a36119b
Greg Tan <greg@gregtan.com>
2021-12-12 21:55:22 +0800

    Added solution to Day 4 part 2

--- aoc4/Program.cs
+++ aoc4/Program.cs
@@ -26,6 +26,30 @@ static class Program {
         Console.WriteLine($"Unmarked nums: {string.Join(",", unmarkedNums.Select(n => n.ToString()))}");
         Console.WriteLine($"Unmarked sum: {unmarkedNumSum}");
         Console.WriteLine($"Score: {unmarkedNumSum * lastNumDrawn}");
+
+        var (numsSelected2, winningBoards2) = drawnNums
+            .Scan((DrawnNums: ImmutableArray<int>.Empty, Boards: boards), (x, n) => {
+                if (x.Boards.Count() == 1) {
+                    return (n, x.Boards);
+                }
+                return (n, x.Boards.Where(b => !b.Win(n.ToImmutableHashSet())));
+            })
+            .SkipWhile(x => x.Boards.Count() > 1)
+            .SkipWhile(x => !x.Boards.First().Win(x.DrawnNums.ToImmutableHashSet()))
+            .Take(1)
+            .FirstAsync().Wait();
+
+        var winningBoard2 = winningBoards2.First();
+        var lastNumDrawn2 = numsSelected2.Last();
+        var unmarkedNums2 = winningBoard2.Unmarked(numsSelected2.ToImmutableHashSet());
+        var unmarkedNumSum2 = unmarkedNums2.Sum();
+
+        Console.WriteLine();
+        Console.WriteLine($"Last num drawn for last winning board: {lastNumDrawn2}");
+        Console.WriteLine($"Last Winning Board:\n{winningBoard2}");
+        Console.WriteLine($"Last Winning Board Unmarked nums: {string.Join(",", unmarkedNums2.Select(n => n.ToString()))}");
+        Console.WriteLine($"Last Winning Board Unmarked sum: {unmarkedNumSum2}");
+        Console.WriteLine($"Last Winning Board Score: {unmarkedNumSum2 * lastNumDrawn2}");
     }
 
     static (IEnumerable<int> DrawnNumbers, IEnumerable<Board> Boards) ReadData() {
