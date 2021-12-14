c7d267cce5ed4eb7156ec8d88880363edf8a86c1
Greg Tan <greg@gregtan.com>
2021-12-14 21:33:32 +0800

    Solution for Day 9, part 1

---
+++ aoc9/Point.cs
@@ -0,0 +1,3 @@
+namespace aoc9;
+
+public readonly record struct Point(int Row, int Col);
---
+++ aoc9/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.IO;
+
+namespace aoc9;
+
+class Program {
+    static void Main(string[] args) {
+        var heights = ReadData();
+
+        var adjacencies = Enumerable.Range(0, heights.Length)
+            .SelectMany(r => Enumerable.Range(0, heights[0].Length).Select(c => new Point(r, c)))
+            .Select(x => (Point: x, Height: heights[x.Row][x.Col], AdjacentPoints: AdjacentPoints(heights, x.Row, x.Col)));
+
+        var lowPoints = adjacencies.Where(x => x.AdjacentPoints.All(a => heights[x.Point.Row][x.Point.Col] < heights[a.Row][a.Col]))
+            .Select(x => x.Point);
+
+        var riskLevels = lowPoints.Select(p => heights[p.Row][p.Col] + 1);
+
+        Console.WriteLine($"Sum of Risk Levels: {riskLevels.Sum()}");
+    }
+
+    private static IEnumerable<Point> AdjacentPoints(ImmutableArray<ImmutableArray<int>> heightMap, int row, int col) {
+        var numRows = heightMap.Length;
+        var numCols = heightMap[0].Length;
+
+        if (row > 0) {
+            yield return new Point(row - 1, col);
+        }
+        if (row < numRows - 1) {
+            yield return new Point(row + 1, col);
+        }
+        if (col > 0) {
+            yield return new Point(row, col - 1);
+        }
+        if (col < numCols - 1) {
+            yield return new Point(row, col + 1);
+        }
+    }
+
+    private static IEnumerable<int> AdjacentHeights(ImmutableArray<ImmutableArray<int>> heightMap, int row, int col) {
+        var numRows = heightMap.Length;
+        var numCols = heightMap[0].Length;
+
+        var builder = ImmutableArray.CreateBuilder<int>();
+
+        if (row > 0) {
+            builder.Add(heightMap[row - 1][col]);
+        }
+        if (row < numRows - 1) {
+            builder.Add(heightMap[row + 1][col]);
+        }
+        if (col > 0) {
+            builder.Add(heightMap[row][col - 1]);
+        }
+        if (col < numCols - 1) {
+            builder.Add(heightMap[row][col + 1]);
+        }
+        return builder.ToImmutableArray();
+    }
+
+    private static ImmutableArray<ImmutableArray<int>> ReadData() {
+        var builder = ImmutableArray.CreateBuilder<ImmutableArray<int>>();
+
+        foreach (var line in File.ReadLines("input.txt")) {
+            builder.Add(line.Select(c => Convert.ToInt32(c.ToString())).ToImmutableArray());
+        }
+        return builder.ToImmutableArray();
+    }
+}
+
