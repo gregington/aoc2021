5e15e0e8d7fa9deb512649b5b0c4a61b0a6d489d
Greg Tan <greg@gregtan.com>
2021-12-14 21:59:28 +0800

    Added Day 9 part 2.

--- aoc9/Program.cs
+++ aoc9/Program.cs
@@ -10,7 +10,7 @@ class Program {
 
         var adjacencies = Enumerable.Range(0, heights.Length)
             .SelectMany(r => Enumerable.Range(0, heights[0].Length).Select(c => new Point(r, c)))
-            .Select(x => (Point: x, Height: heights[x.Row][x.Col], AdjacentPoints: AdjacentPoints(heights, x.Row, x.Col)));
+            .Select(x => (Point: x, Height: heights[x.Row][x.Col], AdjacentPoints: AdjacentPoints(x, heights)));
 
         var lowPoints = adjacencies.Where(x => x.AdjacentPoints.All(a => heights[x.Point.Row][x.Point.Col] < heights[a.Row][a.Col]))
             .Select(x => x.Point);
@@ -18,45 +18,46 @@ class Program {
         var riskLevels = lowPoints.Select(p => heights[p.Row][p.Col] + 1);
 
         Console.WriteLine($"Sum of Risk Levels: {riskLevels.Sum()}");
+
+        var basins = lowPoints.Select(p => FindBasin(p, ImmutableHashSet<Point>.Empty, heights));
+
+        var top3Basins = basins.Select(b => b.Count()).OrderByDescending(c => c).Take(3);
+
+        Console.WriteLine($"Top 3 basins multiplied: {top3Basins.Aggregate(1, (a, b) => a * b)}");
     }
 
-    private static IEnumerable<Point> AdjacentPoints(ImmutableArray<ImmutableArray<int>> heightMap, int row, int col) {
+    static IEnumerable<Point> FindBasin(Point point, ImmutableHashSet<Point> seenPoints, ImmutableArray<ImmutableArray<int>> heights) {
+        seenPoints = seenPoints.Add(point);
+
+        foreach (var adj in AdjacentPoints(point, heights)) {
+            if (seenPoints.Contains(adj)) {
+                continue;
+            }
+            var height = heights[adj.Row][adj.Col];
+            if (height < 9) {
+                seenPoints = seenPoints.Union(FindBasin(adj, seenPoints, heights));
+            }
+        }
+
+        return seenPoints;
+    }
+
+    private static IEnumerable<Point> AdjacentPoints(Point p, ImmutableArray<ImmutableArray<int>> heightMap) {
         var numRows = heightMap.Length;
         var numCols = heightMap[0].Length;
 
-        if (row > 0) {
-            yield return new Point(row - 1, col);
+        if (p.Row > 0) {
+            yield return new Point(p.Row - 1, p.Col);
         }
-        if (row < numRows - 1) {
-            yield return new Point(row + 1, col);
+        if (p.Row < numRows - 1) {
+            yield return new Point(p.Row + 1, p.Col);
         }
-        if (col > 0) {
-            yield return new Point(row, col - 1);
+        if (p.Col > 0) {
+            yield return new Point(p.Row, p.Col - 1);
         }
-        if (col < numCols - 1) {
-            yield return new Point(row, col + 1);
+        if (p.Col < numCols - 1) {
+            yield return new Point(p.Row, p.Col + 1);
         }
-    }
-
-    private static IEnumerable<int> AdjacentHeights(ImmutableArray<ImmutableArray<int>> heightMap, int row, int col) {
-        var numRows = heightMap.Length;
-        var numCols = heightMap[0].Length;
-
-        var builder = ImmutableArray.CreateBuilder<int>();
-
-        if (row > 0) {
-            builder.Add(heightMap[row - 1][col]);
-        }
-        if (row < numRows - 1) {
-            builder.Add(heightMap[row + 1][col]);
-        }
-        if (col > 0) {
-            builder.Add(heightMap[row][col - 1]);
-        }
-        if (col < numCols - 1) {
-            builder.Add(heightMap[row][col + 1]);
-        }
-        return builder.ToImmutableArray();
     }
 
     private static ImmutableArray<ImmutableArray<int>> ReadData() {
