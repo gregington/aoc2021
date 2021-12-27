e753cb2e716fbb8723dcfce439c1bd54b045c199
Greg Tan <greg@gregtan.com>
2021-12-27 14:10:47 +0800

    Solution day 21, part 2.

--- aoc21/Program.cs
+++ aoc21/Program.cs
@@ -11,6 +11,65 @@ class Program {
         var (scores, rolls) = Play(startingPositions);
         var losingScore = scores.Min();
         Console.WriteLine($"Losing score: {losingScore}, Total Rolls: {rolls}, losing score * rolls: {losingScore * rolls}");
+
+        var numWins = PlayQuantum(startingPositions);
+        Console.WriteLine($"Most wins: {numWins.Max()}");
+    }
+
+    private static ImmutableArray<ulong> PlayQuantum(ImmutableArray<int> startingPositions) {
+        var dicePossibilities = CalculateDiracDicePossibilities();
+
+        var winCounts = new ulong[2];
+
+        RollDiracDice(dicePossibilities, 0, 0, startingPositions[0], startingPositions[1], 0, 1, winCounts);
+
+        return winCounts.ToImmutableArray();
+    }
+
+    public static void RollDiracDice(ImmutableDictionary<int, ulong> dicePossibilities, int p0Points, int p1Points, int p0Pos, int p1Pos, int turn, ulong universes, ulong[] winCounts) {
+        const int winningScore = 21;
+        if (p0Points >= winningScore || p1Points >= winningScore) {
+            return;
+        }
+        
+        var wins = new ulong[2];
+
+        if (turn == 0) {
+            foreach (var kvp in dicePossibilities) {
+                var nextPosition = Move(p0Pos, kvp.Key);
+                var newPoints = p0Points + nextPosition;
+                if (newPoints < winningScore) {
+                    RollDiracDice(dicePossibilities, newPoints, p1Points, nextPosition, p1Pos, 1, kvp.Value * universes, winCounts);
+                } else {
+                    winCounts[0] += universes * kvp.Value;
+                }
+            }
+        } else {
+            foreach (var kvp in dicePossibilities) {
+                var nextPosition = Move(p1Pos, kvp.Key);
+                var newPoints = p1Points + nextPosition;
+                if (newPoints < winningScore) {
+                    RollDiracDice(dicePossibilities, p0Points, newPoints, p0Pos, nextPosition, 0, kvp.Value * universes, winCounts);
+                } else {
+                    winCounts[1] += universes * kvp.Value;
+                }
+            }
+        }
+    }
+
+    private static ImmutableDictionary<int, ulong> CalculateDiracDicePossibilities() {
+        var dict = new Dictionary<int, ulong>();
+        for (var i = 1; i <= 3; i++) {
+            for (var j = 1; j <= 3; j++) {
+                for (var k = 1; k <= 3; k++) {
+                    var total = i + j + k;
+                    var prevValue = dict.GetValueOrDefault(total, (ulong) 0);
+                    dict[total] = prevValue + 1;
+                }
+            }
+        }
+
+        return dict.ToImmutableDictionary();
     }
 
     private static (ImmutableArray<int> Scores, int NumRolls) Play(ImmutableArray<int> startingPositions) {
